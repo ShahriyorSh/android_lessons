0ecf3d35e16e3e06401fe2009c3e7d4e80780ad0
Shahriyor Shokirov <shahriyor030387@gmail.ru>
2020-11-23 17:20:24 -0800

    + initial commit 2

--- app/src/main/java/com/example_ilmkhona/MainActivity.java
+++ app/src/main/java/com/example_ilmkhona/MainActivity.java
@@ -2,13 +2,30 @@ package com.example_ilmkhona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt = findViewById(R.id.play_game);
+
+            bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getBaseContext(), "STARTING GAME" , Toast.LENGTH_SHORT ).show();
+                }
+            });
+
+
+
+
     }
 }
